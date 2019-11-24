2fab19936c087c50b54c9622ff64e5ac95d239cd
Krzysztof Sarna <krzysztof.sarna@gmail.com>
2019-11-24 01:30:07 +0100

    Updates

---
+++ precache-manifest.1a973b959a4f9a35d810c76effebf90f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dacc7c7049dab3b5eafaca33316dabf4",
+    "url": "/meetings-callendar/index.html"
+  },
+  {
+    "revision": "7c9e4c848f828767c783",
+    "url": "/meetings-callendar/static/css/main.a5b73f6b.chunk.css"
+  },
+  {
+    "revision": "6415238f26f9716b3a4e",
+    "url": "/meetings-callendar/static/js/2.21b92382.chunk.js"
+  },
+  {
+    "revision": "7c9e4c848f828767c783",
+    "url": "/meetings-callendar/static/js/main.3db43462.chunk.js"
+  },
+  {
+    "revision": "90fc0a452c2b2c34baf4",
+    "url": "/meetings-callendar/static/js/runtime-main.463a38f9.js"
+  }
+]);
